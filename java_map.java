50c44969f57a6c9a7bee534aca61674f18d02053
Thomás Dixini <thomasdixini@gmail.com>
2024-03-05 21:42:37 -0300

    :sparkles: feat: Conferindo se a query existe no meu map e retornando um Map do tipo pessoa=numero_telefone

--- java_map.java
+++ java_map.java
@@ -4,24 +4,36 @@ import java.util.*;
 class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
         Scanner scanner = new Scanner(System.in);
         int qtdNumbers = scanner.nextInt();
 
-        Scanner scanner2 = new Scanner(System.in);
-        String name = scanner2.nextLine();
-
-        Scanner scanner3 = new Scanner(System.in);
-        int phoneNumber = scanner3.nextInt();
-
         Map<String, Integer> map = new HashMap<String, Integer>();
-        map.put(name, phoneNumber);
 
-        boolean hasKey = map.containsKey("Teste");
-        if(!hasKey){
-            System.out.println("Not found");
+        for (int i = 0; i < qtdNumbers; i++) {
+            Scanner scanner2 = new Scanner(System.in);
+            String name = scanner2.nextLine();
+
+            Scanner scanner3 = new Scanner(System.in);
+            int phoneNumber = scanner3.nextInt();
+
+            map.put(name, phoneNumber);
         }
 
-        System.out.println(map);
+        for (Map.Entry<String, Integer> item : map.entrySet()) {
+            Scanner scanner4 = new Scanner(System.in);
+            String query = scanner4.nextLine();
+
+            boolean found = map.containsKey(query);
+
+            if (!found) {
+                System.out.println("Not found");
+            } else {
+                System.out.println(item);
+            }
+        }
     }
 }
