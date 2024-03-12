aced19253f0d77961a8807213ca7fbe2076c3320
Thomás Dixini <thomasdixini@gmail.com>
2024-03-11 21:19:23 -0300

    :ok_hand: #2 Simple input: Desafio concluído

---
+++ Simple_input_output.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+
+    public static void main(String[] args) {
+        /*
+         * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+         * class should be named Solution.
+         */
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        int third = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(String.format("%d \n%d \n%d", first, second, third));
+    }
+}
